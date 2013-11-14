57e303ba1e0dfa80af7bf10d935a602721657b0d
Kujbor <Kujbor@ya.ru>
2013-11-14 17:51:20 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,10 +1,8 @@
 /*
- * example.js - example of jQuery Back-to-top button plugin initialization
+ * Example of jQuery Back-to-top button plugin initialization
  * Author Oleg Taranov aka Kujbor
  * Copyright (C) 2013: CubeComp Development
  */
 $(function() {
-
     $('#backToTop').backToTop('.container');
-    
 });
